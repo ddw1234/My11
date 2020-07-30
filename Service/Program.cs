14f838f16bbfb5640f0b4bd4fc973a519dd3a4ad
Dinesh-PC <dineshwagh777@gmail.com>
2020-07-30 12:07:33 +0530

    added asln file

---
+++ Service/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            S obj = new K();
+            obj.play();
+
+           //K kb = obj.play();
+
+            Console.ReadLine();
+           
+        }
+
+        class S
+        {
+            
+           public virtual S play()
+            {
+                return new S();
+            }
+        }
+        class K:S
+        {
+            public override S play()
+            {
+                return new K();
+            }
+        }
+    }
+}
